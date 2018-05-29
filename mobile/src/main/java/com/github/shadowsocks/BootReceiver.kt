7685c86566767bf1a769248587ee41d263f6f556
dellzhui <dellzhui@gmail.com>
2018-05-29 19:02:38 +0800

    add monitor support stage 1

--- mobile/src/main/java/com/github/shadowsocks/BootReceiver.kt
+++ mobile/src/main/java/com/github/shadowsocks/BootReceiver.kt
@@ -31,6 +31,13 @@ import com.github.shadowsocks.App.Companion.app
 import com.github.shadowsocks.preference.DataStore
 import java.io.IOException
 import com.inspur.reflect.local_reflect
+import android.net.NetworkInfo
+import android.net.ConnectivityManager
+import android.R.attr.action
+import android.os.AsyncTask
+
+
+
 
 class BootReceiver : BroadcastReceiver() {
     companion object {
@@ -51,14 +58,7 @@ class BootReceiver : BroadcastReceiver() {
             set(value) = enabled_local_set(value)
     }
 
-    override fun onReceive(context: Context, intent: Intent) {
-		Log.e(TAG, "intent.action is " + intent.action)
-        val locked = when (intent.action) {
-            Intent.ACTION_BOOT_COMPLETED -> false
-            Intent.ACTION_LOCKED_BOOT_COMPLETED -> true // constant will be folded so no need to do version checks
-            else -> return
-        }
-
+    private fun init_database(context: Context) {
         try {
             var index: Int = 0
             val timeout: Int = 10
@@ -84,8 +84,93 @@ class BootReceiver : BroadcastReceiver() {
             Log.e(TAG, "init database failed")
             ex.printStackTrace()
         }
+    }
 
+    private inner class MonitorTaskClass : AsyncTask<String, Void, String>() {
+        override fun doInBackground(vararg params: String): String {
+            perform_monitor_task()
+            return "Executed"
+        }
+        override fun onPreExecute() {}
+        override fun onProgressUpdate(vararg values: Void) {}
+    }
+
+    private fun perform_monitor_task() {
+        Log.e(TAG, "monitor task started")
+        System.setProperty("proxy.monitor.status", "running")
+        Thread.sleep(1000 * 10)
+        Log.e(TAG, "we will reload service")
+        app.reloadService()
+        Thread.sleep(1000 * 1000)
+        //System.setProperty("proxy.monitor.status", "stoped")
+        //Log.e(TAG, "monitor task end")
+    }
+
+    private fun start_client_task() {
+        val proxy_monitor_status = System.getProperty("proxy.monitor.status")
+        Log.e(TAG, "proxy_monitor_status is " + proxy_monitor_status)
+
+        if(proxy_monitor_status == "running") {
+            Log.e(TAG, "already started")
+            return
+        }
+
+        if(System.getProperty("proxy.monitor.boot") == "received" && System.getProperty("proxy.monitor.net_connect") == "received") {
+            Log.e(TAG, "we will start monitor task")
+            MonitorTaskClass().execute("")
+            return
+        }
+        Log.e(TAG, "we should start only after booting and network connected")
+    }
+
+    private fun handle_intent(context: Context, intent: Intent) {
+        val action = intent.action
+
+        if(action == Intent.ACTION_BOOT_COMPLETED) {
+            init_database(context)
+            app.startService()
+            System.setProperty("proxy.monitor.boot", "received")
+            start_client_task()
+            return
+        }
+
+        if (action.equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+            Log.d(TAG, "get CONNECTIVITY_ACTION")
+            val netType = intent.getIntExtra(ConnectivityManager.EXTRA_NETWORK_TYPE, 9)
+            Log.d(TAG, "netType_str is $netType")
+
+            if(netType == ConnectivityManager.TYPE_VPN) {
+                Log.e(TAG, "ignore TYPE_VPN")
+                return
+            }
+
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            Log.d(TAG, "mConnectivityManager is $connectivityManager")
+
+            val mInfo = connectivityManager.getNetworkInfo(netType)
+            Log.d(TAG, "mInfo is " + mInfo)
+            if (mInfo.isAvailable()) {
+                val state = mInfo.getDetailedState()
+                Log.d(TAG, "state is $state")
+                if(state == NetworkInfo.DetailedState.CONNECTED) {
+                    System.setProperty("proxy.monitor.net_connect", "received")
+                    start_client_task()
+                }
+            } else {
+                Log.d(TAG, "no avaliable network")
+            }
+        }
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.e(TAG, "intent.action is " + intent.action)
+        /*val locked = when (intent.action) {
+            Intent.ACTION_BOOT_COMPLETED -> false
+            Intent.ACTION_LOCKED_BOOT_COMPLETED -> true // constant will be folded so no need to do version checks
+            else -> return
+        }
         Log.e(TAG, "DataStore.directBootAware is " + DataStore.directBootAware + ", locked is " + locked)
-        if (DataStore.directBootAware == locked) app.startService()
+        if (DataStore.directBootAware == locked) app.startService()*/
+        handle_intent(context, intent)
     }
 }
