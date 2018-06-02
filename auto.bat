5214d14c88cb0d7b22a80e1e7d8fcd0814d21fe9
chenfx <chenfx@inspur.com>
2018-06-02 14:48:51 +0800

    update auto.bat

--- auto.bat
+++ auto.bat
@@ -1,4 +1,4 @@
-java -jar key/signapk.jar key/platform.x509.pem key/platform.pk8 mobile/build/outputs/apk/debug/mobile-armeabi-v7a-debug.apk mobile-armeabi-v7a_plat.apk
+java -jar key/signapk.jar key/platform.x509.pem key/platform.pk8 mobile/build/outputs/apk/debug/mobile-armeabi-v7a-debug.apk appproxy.apk
 
 ::adb shell pm uninstall com.github.appproxy
 
