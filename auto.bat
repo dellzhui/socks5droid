1edf509a1487c03508e425a68627fd31d5c29f17
chenfx <chenfx@inspur.com>
2022-06-29 19:24:12 +0800

    update auto.sh

--- auto.bat
+++ auto.bat
@@ -1,9 +1,17 @@
 java -jar key/signapk.jar key/platform.x509.pem key/platform.pk8 mobile/build/outputs/apk/debug/mobile-debug.apk appproxy.apk
 
-adb shell pm uninstall com.github.appproxy
+adb shell remount.sh
 
-adb push appproxy.apk /data/local/tmp/com.github.appproxy
+adb push appproxy.apk /system/priv-app/
 
-adb shell pm install -t -r "/data/local/tmp/com.github.appproxy"
+adb shell rm -rf /data/data/com.github.appproxy
+adb shell sync
 
-:: adb shell am start -n "com.github.appproxy/com.github.appproxy.MainActivity"
+
+:: adb shell pm uninstall com.github.appproxy
+
+:: adb push appproxy.apk /data/local/tmp/com.github.appproxy
+
+:: adb shell pm install -t -r "/data/local/tmp/com.github.appproxy"
+
+:: adb shell am start -n "com.github.appproxy/com.github.appproxy.VpnRequestActivity"
